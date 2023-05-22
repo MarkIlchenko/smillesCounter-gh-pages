82b4f88a0ba58ad590a0517c391707b30c5b5cb8
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-05-22 10:16:56 +0300

    First commit

--- src/components/Smilles.js
+++ src/components/Smilles.js
@@ -11,9 +11,9 @@ const EmoticonList = () => {
             setEmoticons(storedEmoticons);
         } else {
             const defaultEmoticons = [
-                { id: 1, name: 'Grinning Face', count: 0 },
-                { id: 2, name: 'Smiling Face with Sunglasses', count: 0 },
-                { id: 3, name: 'Face with Rolling Eyes', count: 0 },
+                { id: 1, name: '😀', count: 0 },
+                { id: 2, name: '😂', count: 0 },
+                { id: 3, name: '🥲', count: 0 },
             ];
             setEmoticons(defaultEmoticons);
             localStorage.setItem('emoticons', JSON.stringify(defaultEmoticons));
